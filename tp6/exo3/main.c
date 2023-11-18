cc56722c7adfc8db61c5ba2172a744c178a66f27
= <sita.kagnon@gmail.com>
2023-11-18 21:44:09 +0100

    final

--- tp6/exo3/main.c
+++ tp6/exo3/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 #include "hanoi.h"
 
 
@@ -11,6 +12,22 @@ int main(int argc, char* argv[]){
     printf("Tour %c - Tour %c - Tour %c\n",argv[1][0],argv[2][0],argv[3][0]);
     Hanoi(4,argv[1][0],argv[2][0],argv[3][0]);
 
+    /*SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *fenetre;
+    SDL_Renderer *rendu;
+    SDL_CreateWindowAndRenderer(800, 300, 0, &fenetre, &rendu);
+
+    int nombreDeDisques;
+    printf("Entrez le nombre de disques : ");
+    scanf("%d", &nombreDeDisques);
+
+    Hanoi_graph(rendu, nombreDeDisques, 200, 400, 600);
+
+    return 0;*/
+
+
 
     return 0;
 }
+
