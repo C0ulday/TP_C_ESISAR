cbadd01250774162e529abc59ee927aafd69065a
= <sita.kagnon@gmail.com>
2023-11-18 21:44:09 +0100

    final

--- tp6/exo3/hanoi.h
+++ tp6/exo3/hanoi.h
@@ -2,5 +2,7 @@
 #define __HANOI_C__
 
 void Hanoi(int n, char tour1,char tour2,char tour3);
-
+void dessinerPoteau(SDL_Renderer *renderer, int x);
+void dessinerDisque(SDL_Renderer *renderer, int x, int y, int largeur);
+void Hanoi_graph(SDL_Renderer* renderer,int n, int tour1, int tour2, int tour3);
 #endif
